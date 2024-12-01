7368853ba504290d3c9698b5b5243b8cfd20d11a
hsk-2004 <harman.singh.23cse@bmu.edu.in>
2024-12-01 14:16:54 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+// Hash password before saving (bcrypt is used here)
+const bcrypt = require('bcrypt');
+
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
